10bcb96f068521ef9a3855e5175b166525055576
Muhammad Umair <hafizumairtariq1234@gmail.com>
2021-05-31 12:16:54 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import QuizContainer from './components/QuizContainer/QuizContainer';
+import { GlobalStyle } from './App.style';
+import { QuizProvider } from './context/QuizContext';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <QuizProvider>
+      <GlobalStyle/>
+      <QuizContainer/>
+    </QuizProvider>
+  )
 }
 
-export default App;
+export default App
