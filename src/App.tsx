07884153ae84b13df6df3d185d0a90f33659110b
Muhammad Umair <hafizumairtariq1234@gmail.com>
2021-06-02 23:54:59 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 import QuizContainer from './components/QuizContainer/QuizContainer';
 import { GlobalStyle } from './App.style';
 import { QuizProvider } from './context/QuizContext';
-import firebase from './firebase';
 
 const App = () => {
-  const messaging=firebase.messaging();
-  messaging.requestPermission()
-  .then(()=>messaging.getToken())
-  .then((token:any)=>console.log("Token",token));
   return (
     <QuizProvider>
       <GlobalStyle/>
